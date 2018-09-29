a02d4130eee801e8e5205f8fc62369a0104193c5
Han Zheng <hzheng12@stevens.edu>
2018-09-29 10:50:46 -0400

    Refactored a type name
    1. "shift::Order::ORDER_TYPE" to "shift::Order::Type"

--- include/Order.h
+++ include/Order.h
@@ -11,7 +11,7 @@ void bindPython(py::module& m)
     //! Bind class
     py::class_<shift::Order> ORDER(m, "Order");
     ORDER //! Bind constructor
-        .def(py::init<std::string, double, int, shift::Order::ORDER_TYPE>(), py::arg("symbol"), py::arg("price"), py::arg("size"), py::arg("type"));
+        .def(py::init<std::string, double, int, shift::Order::Type>(), py::arg("symbol"), py::arg("price"), py::arg("size"), py::arg("type"));
     ORDER //! Bind property
         .def_property("symbol", &shift::Order::getSymbol, &shift::Order::setSymbol, py::return_value_policy::reference)
         .def_property("price", &shift::Order::getPrice, &shift::Order::setPrice, py::return_value_policy::reference)
@@ -20,14 +20,14 @@ void bindPython(py::module& m)
         .def_property("id", &shift::Order::getID, &shift::Order::setID, py::return_value_policy::reference);
 
     //! Bind enum type
-    py::enum_<shift::Order::ORDER_TYPE> ORDER_TYPE(ORDER, "OrderType");
-    ORDER_TYPE
-        .value("CANCEL_ASK", shift::Order::ORDER_TYPE::CANCEL_ASK)
-        .value("CANCEL_BID", shift::Order::ORDER_TYPE::CANCEL_BID)
-        .value("LIMIT_BUY", shift::Order::ORDER_TYPE::LIMIT_BUY)
-        .value("LIMIT_SELL", shift::Order::ORDER_TYPE::LIMIT_SELL)
-        .value("MARKET_BUY", shift::Order::ORDER_TYPE::MARKET_BUY)
-        .value("MARKET_SELL", shift::Order::ORDER_TYPE::MARKET_SELL)
+    py::enum_<shift::Order::Type> Type(ORDER, "OrderType");
+    Type
+        .value("CANCEL_ASK", shift::Order::Type::CANCEL_ASK)
+        .value("CANCEL_BID", shift::Order::Type::CANCEL_BID)
+        .value("LIMIT_BUY", shift::Order::Type::LIMIT_BUY)
+        .value("LIMIT_SELL", shift::Order::Type::LIMIT_SELL)
+        .value("MARKET_BUY", shift::Order::Type::MARKET_BUY)
+        .value("MARKET_SELL", shift::Order::Type::MARKET_SELL)
         .export_values();
 }
 
