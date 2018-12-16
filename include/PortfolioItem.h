de017064c69d5a806848ded16fe0cd2e00d9795a
Thiago W. Alves <twinkle1@stevens.edu>
2018-12-16 14:10:03 -0500

    Adding separate access to Long/Short Shares/Price information; Hiding OpenBP information (since it cannot be used by Python Client)

--- include/PortfolioItem.h
+++ include/PortfolioItem.h
@@ -14,11 +14,15 @@ void bindPython(py::module& m)
     py::class_<shift::PortfolioItem> PORTFOLIO_ITEM(m, "PortfolioItem");
     PORTFOLIO_ITEM //! Bind constructor
         .def(py::init<>())
-        .def(py::init<std::string, int, double, double>());
+        .def(py::init<std::string, int, int, double, double, double>());
     PORTFOLIO_ITEM //! Bind function
         .def("getSymbol", &shift::PortfolioItem::getSymbol)
         .def("getShares", &shift::PortfolioItem::getShares)
+        .def("getLongShares", &shift::PortfolioItem::getLongShares)
+        .def("getShortShares", &shift::PortfolioItem::getShortShares)
         .def("getPrice", &shift::PortfolioItem::getPrice)
+        .def("getLongPrice", &shift::PortfolioItem::getLongPrice)
+        .def("getShortPrice", &shift::PortfolioItem::getShortPrice)
         .def("getRealizedPL", &shift::PortfolioItem::getRealizedPL)
         .def("getTimestamp", &shift::PortfolioItem::getTimestamp);
 }
--- include/PortfolioSummary.h
+++ include/PortfolioSummary.h
@@ -12,10 +12,10 @@ void bindPython(py::module& m)
     py::class_<shift::PortfolioSummary> PORTFOLIO_SUMMARY(m, "PortfolioSummary");
     PORTFOLIO_SUMMARY //! Bind constructor
         .def(py::init<double, int, double>(), py::arg("totalBP"), py::arg("totalShares"), py::arg("totalRealizedPL"));
-    PORTFOLIO_SUMMARY //! Bind function
-        .def("isOpenBPReady", &shift::PortfolioSummary::isOpenBPReady);
+    // PORTFOLIO_SUMMARY //! Bind function
+    //     .def("isOpenBPReady", &shift::PortfolioSummary::isOpenBPReady);
     PORTFOLIO_SUMMARY //! Bind property
-        .def_property("openBP", &shift::PortfolioSummary::getOpenBP, &shift::PortfolioSummary::setOpenBP, py::return_value_policy::reference)
+        // .def_property("openBP", &shift::PortfolioSummary::getOpenBP, &shift::PortfolioSummary::setOpenBP, py::return_value_policy::reference)
         .def_property("totalBP", &shift::PortfolioSummary::getTotalBP, &shift::PortfolioSummary::setTotalBP, py::return_value_policy::reference)
         .def_property("totalShares", &shift::PortfolioSummary::getTotalShares, &shift::PortfolioSummary::setTotalShares, py::return_value_policy::reference)
         .def_property("totalRealizedPL", &shift::PortfolioSummary::getTotalRealizedPL, &shift::PortfolioSummary::setTotalRealizedPL, py::return_value_policy::reference)
