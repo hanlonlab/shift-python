a9d0087bc8f2ac87bdb91b0c6b8863d043a3f471
Thiago W. Alves <twinkle1@stevens.edu>
2019-01-25 18:21:26 -0500

    Temporary fix so that we can still compile with the current version of LC

--- include/OrderBookEntry.h
+++ include/OrderBookEntry.h
@@ -10,12 +10,12 @@ void bindPython(py::module& m)
 {
     //! Bind class
     py::class_<shift::OrderBookEntry> ORDER_BOOK_ENTRY(m, "OrderBookEntry");
-    ORDER_BOOK_ENTRY //! Bind constructor
-        .def(py::init<double, double, double, std::string>(), py::arg("price"), py::arg("size"), py::arg("time"), py::arg("destination"));
+    // ORDER_BOOK_ENTRY //! Bind constructor
+    //     .def(py::init<double, double, double, std::string>(), py::arg("price"), py::arg("size"), py::arg("time"), py::arg("destination"));
     ORDER_BOOK_ENTRY //! Bind property
         .def_property("price", &shift::OrderBookEntry::getPrice, &shift::OrderBookEntry::setPrice, py::return_value_policy::reference)
         .def_property("size", &shift::OrderBookEntry::getSize, &shift::OrderBookEntry::setSize, py::return_value_policy::reference)
-        .def_property("time", &shift::OrderBookEntry::getTime, &shift::OrderBookEntry::setTime, py::return_value_policy::reference)
+        // .def_property("time", &shift::OrderBookEntry::getTime, &shift::OrderBookEntry::setTime, py::return_value_policy::reference)
         .def_property("destination", &shift::OrderBookEntry::getDestination, &shift::OrderBookEntry::setDestination, py::return_value_policy::reference);
 }
 
