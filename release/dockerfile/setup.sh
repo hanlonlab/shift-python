27fd168f86f491d63a3b4074f5716c8b3c19460a
Thiago W. Alves <twinkle1@stevens.edu>
2019-02-15 23:03:57 -0500

    Reorganizing docker files

--- release/dockerfile/setup.sh
+++
@@ -1,6 +0,0 @@
-conda create -y -n shift python=3.7 anaconda
-source /opt/conda/etc/profile.d/conda.sh
-conda activate shift
-wget -qO- https://github.com/hanlonlab/shift-python/releases/download/0.0.1/shift-python_0.0.1_conda_linux64_py37.zip | bsdtar -xvf-
-cd shift*
-conda install *.tar.bz2
