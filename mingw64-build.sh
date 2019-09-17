f39c0f9e9d0473a3fd519a5f98a2f91c99c18e9f
Thiago Winkler Alves <twinkle1@stevens.edu>
2019-09-17 17:05:25 -0400

    Removing Windows-related build scripts, since these do not work anyway.

--- mingw64-build.sh
+++
@@ -1,8 +0,0 @@
-/c/Win-builds/bin/c++.exe -O2 -Wall -shared -std=c++11 -fPIC \
--IC:/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/Shared/Python37_64/include \
--IC:/pybind11/include \
--I../LibCoreClient/include -I./include \
--L../LibCoreClient/build/Release \
--lshift_coreclient \
-src/SHIFT.cpp src/Trader.cpp \
--o build/shift.dll
--- win-build-pybind11x86.sh
+++
@@ -1,5 +0,0 @@
-cd pybind11
-rm -r build
-
-cmake -H. -B./build -G"Visual Studio 15 2017"
-cmake --build ./build --config Release --target check
